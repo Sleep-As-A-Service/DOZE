8b426d21a8a45a43b5750e5e2000eab6f181b2d1
Bethany Cable <bethany.a.cable@gmail.com>
2022-12-27 16:59:32 -0500

    Rename controller folder, add user base code layout and connect to server.js

--- Server/server.js
+++ Server/server.js
@@ -6,6 +6,10 @@ const PORT = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// TODO: check path
+// import routers
+const usersRouter = require('./routes/users.js');
+
 app.get('/', (req, res) => {
   return res.send('This is a test!');
 });
