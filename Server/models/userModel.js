f9137213adfd6556285d5d25ce4a484cd102ec20
JoshDPT <jjewcu@gmail.com>
2022-12-29 13:32:36 -0500

    updated models

--- Server/models/userModel.js
+++ Server/models/userModel.js
@@ -3,6 +3,7 @@ const mongooseInstance = require('./mongoUtil.js');
 const userSchema = mongooseInstance.Schema(
   {
     username: { type: String, required: true },
+    id: { type: Number, required: true },
   },
   { timestamps: true }
 );
