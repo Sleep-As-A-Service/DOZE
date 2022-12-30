5c32bb7ee068126177aa7368e52c229b7e7b84a9
Stefan Jordan <sjordan2010@gmail.com>
2022-12-30 11:09:40 -0500

    dynamically rendering radio input elements using .map instead of hard-coding

--- Client/Components/Question.jsx
+++ Client/Components/Question.jsx
@@ -2,16 +2,27 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 
 function Question({ question, handleChange, index }) {
+
+    const options = [{id: 'Never', name: question.name, value: 0}, {id: 'Sometimes', name: question.name, value: 2}, {id: 'Always', name: question.name, value: 4}]
+
     return (
         <div>
-            <div>{question.text}</div>
-            <input type="radio" id="never" name={question.name} value={0} index={index} onClick={handleChange} />
-                <label hmtlFor="never">Never</label><br></br>
-            <input type="radio" id="sometimes" name={question.name} value={2} index={index} onClick={handleChange} />
-                <label htmlFor="sometimes">Sometimes</label><br></br>
-            <input type="radio" id="always" name={question.name} value={4} index={index} onClick={handleChange} />
-                <label htmlFor="always">Always</label><br></br>
-        </ div>
+            {options.map(option => (
+                <div key={option.id}>
+                <input type="radio" id={option.id} name={option.name} value={option.value} onChange={handleChange} index={index} />
+                    <label htmlFor={option.id}>{option.id}</label>
+                </div>
+            ))}
+        </div>
+//         <div>
+//             <div>{question.text}</div>
+//             <input type="radio" id="never" name={question.name} value={0} index={index} onClick={handleChange} />
+//                 <label hmtlFor="never">Never</label><br></br>
+//             <input type="radio" id="sometimes" name={question.name} value={2} index={index} onClick={handleChange} />
+//                 <label htmlFor="sometimes">Sometimes</label><br></br>
+//             <input type="radio" id="always" name={question.name} value={4} index={index} onClick={handleChange} />
+//                 <label htmlFor="always">Always</label><br></br>
+//         </ div>
     );
 }
 export default Question;
