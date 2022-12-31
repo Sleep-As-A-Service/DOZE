e6a3b9b2462ec360bff4cc655ccf7fd754f69b2c
Stefan Jordan <sjordan2010@gmail.com>
2022-12-31 14:42:14 -0500

    added more answer options... created onSubmit POST request... adjusted dynamic rendering of questions

--- Client/Components/Question.jsx
+++ Client/Components/Question.jsx
@@ -4,13 +4,15 @@ import React, { useEffect, useState } from "react";
 function Question({ question, handleChange, index }) {
 
     const options = [   
-        {id: 'Never', name: question.name, value: 0}, 
-        {id: 'Sometimes', name: question.name, value: 2}, 
-        {id: 'Always', name: question.name, value: 4}
+        {id: 'Never', name: question.id, value: 0},
+        {id: 'Rarely', name: question.id, value: 1}, 
+        {id: 'Sometimes', name: question.id, value: 2}, 
+        {id: 'Frequently', name: question.id, value: 3},
+        {id: 'Always', name: question.id, value: 4}
     ];
 
     return (
-        <div>
+        <div> {question.text}
             {
                 options.map(option => (
                     <div key={option.id}>
--- Client/Components/Questionnaire.jsx
+++ Client/Components/Questionnaire.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes, useRevalidator } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import Question from "./Question.jsx";
 
@@ -10,53 +10,68 @@ function Questionnaire() {
     const questions = [
         {
         text: 'question 1?',
-        name: 'q1'
+        id: 'q1'
         },
         {
         text: 'question 2?',
-        name: 'q2'
+        id: 'q2'
         },
         {
         text: 'question 3?',
-        name: 'q3'
+        id: 'q3'
         },
         {
         text: 'question 4?',
-        name: 'q4'
+        id: 'q4'
         }
     ];
 
+    const sum = answers.reduce((acc, curr) => {
+        return acc + parseInt(Object.values(curr)[0], 10);
+    }, 0);
+    
+
     function handleChange(event) {
         // OBJECT DECONSTRUCT EVENT.TARGET TO GRAB NAME AND VALUE
-        const { name, value } = event.target;
+        const { id, value } = event.target;
         // CLONE ANSWERS STATE ARRAY SO NOT DIRECTLY MUTATING STATE
         const newArr = [...answers];
-        newArr[currentQuestion] = { [name] : value };
+        newArr[currentQuestion] = { [id] : value };
         setAnswers(newArr);
-        // console.log('totalscore', answers);
+        console.log('totalscore', sum);
     }
     function handleSubmit(event) {
+        event.preventDefault();
+        alert('submitted', answers)
         console.log('submitted');
+        // fetch('/forms', {
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type': 'application/json'
+        //     },
+        //     body: JSON.stringify({
+        //         user: userID,
+        //         answers: answers,
+        //         totalScore: sum
+        //     })
+        //     .then((res) => res.json())
+        //     .then((data) => console.log(data))
+            
+        // })
         // NEED TO ADD LOGIC HERE TO HANDLE SUBMITTING THE FINISHED FORM
-        // REQ.BODY OF POST SHOULD BE ANSWERS ARRAY AND USER ID
+        // REQ.BODY OF POST SHOULD BE USERID, ANSWERS ARRAY, TOTAL SCORE
     }
     function handleNext() {
-        console.log('next');
+        console.log('next', sum);
         // INCREMENT CURRENTQUESTION TO ACCESS NEXT QUESTION BY INDEX IN ARRAY OF QUESTIONS
         setCurrentQuestion(currentQuestion + 1);
         // RESET FORM WHEN CLICKING NEXT BUTTON SO NOTHING IS ALREADY CHECKED
         document.getElementById('questionForm').reset();
-    }
+    };
     // function handlePrev() {
     //     console.log('prev');
     //     setCurrentQuestion(currentQuestion - 1);
     // }
-
-    // JUST FOR VISUALS/TESTING
-    const sum = answers.reduce((acc, curr) => {
-        return acc + parseInt(Object.values(curr)[0], 10);
-      }, 0);
-
 
   return (
     <form id="questionForm" onSubmit={handleSubmit}>
