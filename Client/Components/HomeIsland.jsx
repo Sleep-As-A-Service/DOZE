26ca082eff24d823d9e40a96b77439b0ba6b2108
Bethany Mattern <bethany.a.cable@gmail.com>
2023-01-02 15:40:38 -0500

    Display component to screen and add styling. co-authored-by: Josh Edwards jjewcu@gmail.com co-authored-by: Umair Shafiq Soulofstorms@hotmail.com

--- Client/Components/HomeIsland.jsx
+++ Client/Components/HomeIsland.jsx
@@ -1,18 +1,34 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import {HiOutlineMagnifyingGlass, RiZzzFill, VscNotebook} from 'react-icons';
 import React, { useEffect, useState } from "react";
+
+import { HiMagnifyingGlass } from 'react-icons/hi2';
+import { RiZzzFill } from 'react-icons/ri';
+import { VscNotebook } from 'react-icons/vsc';
 
 const HomeIsland = () => {
     const divData = [
-        {icon: <RiZzzFill />, text: 'Assess your sleep quality'},
-        {icon: <VscNotebookRiZzzFill/>, text: 'Go to your journal'},
-        {icon: <HiOutlineMagnifyingGlass />, text: 'See your data'},
+        {icon: <RiZzzFill size={'2em'} style={{color: '#BB86FC'}}/>, text: 'Assess your sleep quality'},
+        {icon: <VscNotebook size={'2em'} style={{color: '#BB86FC'}}/>, text: 'Go to your journal'},
+        {icon: <HiMagnifyingGlass size={'2em'} style={{color: '#BB86FC'}}/>, text: 'See your data'},
     ];
-  return (
-    <div>
 
+    const dataComponents = divData.map((data, index) => {
+        return (
+            <div 
+                key={index}
+                className="island-buttons"
+            >
+                {data.icon}
+                {data.text}
+            </div>
+        )
+    });
+    
+  return (
+    <div className="island-container">
+        {dataComponents}
     </div>
   )
 }
 
-export default HomeIsland
+export default HomeIsland;
